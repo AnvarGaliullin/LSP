3ee4582a7c35390cfd590504b87a319dd7dfcb37
AnvarGaliullin <55315341+AnvarGaliullin@users.noreply.github.com>
2020-01-16 12:02:18 +0500

    Add seed data

--- application/forms.py
+++ application/forms.py
@@ -7,22 +7,21 @@ from wtforms.widgets import PasswordInput
 import phonenumbers
 
 
-#...
-#...
 class LoginForm(FlaskForm):
     email = StringField("Email:",  validators=[DataRequired(), Email()])
     password = StringField("Пароль:", validators=[DataRequired()], widget=PasswordInput(hide_value=False))
     remember = BooleanField("Запомнить меня:")
     submit = SubmitField("Войти")
 
+
 class RegisterForm(FlaskForm):
     email = StringField("E-mail:",  validators=[DataRequired(), Email()])
-    password = StringField("Пароль:", validators=[Length(min = 4, max = 25, message = 'Password must be between 4 and 25 characters')], widget=PasswordInput(hide_value=False))
+    password = StringField("Пароль:", validators=[Regexp("(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$", message="Слишком слабый пароль"), Length(min = 4, max = 25, message = 'Password must be between 4 and 25 characters')], widget=PasswordInput(hide_value=False))
     confirm_password = StringField("Подтвердите пароль:", validators=[EqualTo('password', message = "Passwords must match")], widget=PasswordInput(hide_value=False))
     registration_password = StringField("Верификационный код:", validators=[DataRequired()])
     submit = SubmitField("Зарегистрироваться")
-#strong password regex
-# (?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$ 
+
+
 class PersonalCabinetForm(FlaskForm):
     email = StringField("E-mail:")# Валидаторов нет - потому что эту форму пользователь не может редактировать
     phone = StringField('Телефон', validators=[Optional(), Regexp("^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$", message="Некорректный формат телефона")], render_kw={"placeholder": "введите номер телефона"} ) 
@@ -34,16 +33,19 @@ class PersonalCabinetForm(FlaskForm):
     instagram = StringField("instagram:", validators=[Optional(), Regexp("http(s)?:\/\/(www\.)?(facebook|fb)\.com\/[A-z0-9_\-\.]+\/?", message="Некорректная ссылка")], render_kw={"placeholder": "ссылка на instagram профиль"} )
     submit = SubmitField("Сохранить изменения")
 
+
 class ChangePassword(FlaskForm):
     old_password = StringField("Старый пароль:", widget=PasswordInput(hide_value=False))
-    new_password = StringField("Новый пароль:", validators=[Length(min = 4, max = 25, message = 'Password must be between 4 and 25 characters')], widget=PasswordInput(hide_value=False))
+    new_password = StringField("Новый пароль:", validators=[Regexp("(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$", message="Слишком слабый пароль"), Length(min = 4, max = 25, message = 'Password must be between 4 and 25 characters')], widget=PasswordInput(hide_value=False))
     confirm_new_password = StringField("Подтвердите пароль:", validators=[EqualTo('new_password', message = "Passwords must match")], widget=PasswordInput(hide_value=False))
     submit = SubmitField("Сменить пароль")
+
 
 class CourseAddMaterialForm(FlaskForm):
     name = StringField("Название:", validators=[Length(min = 1, message = 'Обязательное поле'), Length(max = 100, message = 'Слишком длинное название')])
     content = TextAreaField("Содержание:", validators=[Length(min = 1, message = 'Обязательное поле'), Length(max = 10000, message = 'Слишком длинное содержание')])
     submit = SubmitField("Сохранить")
+
 
 class CourseHometaskForm(FlaskForm):
     name = StringField("Название:", validators=[Length(min = 1, message = 'Обязательное поле'), Length(max = 100, message = 'Слишком длинное название')])
@@ -51,16 +53,14 @@ class CourseHometaskForm(FlaskForm):
     start_dttm = DateTimeLocalField("Начало сдачи", format='%Y-%m-%dT%H:%M', validators=[Required()])
     end_dttm = DateTimeLocalField("Окончание сдачи", format='%Y-%m-%dT%H:%M', validators=[Required()])
     submit = SubmitField("Сохранить")
-
+    # Redefine validate method, added aditional condition
     def validate(self):
-        rv = FlaskForm.validate(self)
-        if not rv:
+        is_valid = FlaskForm.validate(self)
+        if not is_valid:
             return False
-
         if self.start_dttm.data > self.end_dttm.data:
             self.start_dttm.errors.append('Начало сдачи должно быть до дедлайна')
             return False
-
         return True
 
 class StudentHometaskForm(FlaskForm):
--- config.py
+++ config.py
@@ -4,35 +4,23 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 db_base_type = 'postgres'
 db_base_name = 'LMS'
 db_base_password = '1234567890Aa'
-db_path = db_base_type+'://'+db_base_type+':'+db_base_password+'@localhost/'+db_base_name
+db_path = db_base_type + '://' + db_base_type + ':' + db_base_password + '@localhost/' + db_base_name
+
 
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'A SECRET KEY'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # ##### настройка Flask-Mail #####
-    # MAIL_SERVER = 'smtp.googlemail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'YOU_MAIL@gmail.com'
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'password'
-    # MAIL_DEFAULT_SENDER = MAIL_USERNAME
-
 
 class DevelopementConfig(BaseConfig):
     DEBUG = True
-
     SQLALCHEMY_DATABASE_URI = 'postgres://postgres:1234567890Aa@localhost/LMS'
-
-
-
 
 
 # class TestingConfig(BaseConfig):
 #     DEBUG = True
 #     SQLALCHEMY_DATABASE_URI = os.environ.get('TESTING_DATABASE_URI') or \
 # 			      'mysql+pymysql://root:pass@localhost/flask_app_db'
-
 
 # class ProductionConfig(BaseConfig):
 #     DEBUG = False
